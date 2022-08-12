fc62435265832a8efb8891c5c73f7ee2ac9d4289
Matthew Sunner <mattsunner@matthews-air.lan>
2022-08-12 13:19:31 -0500

    Install Pandas and Plotly; Start Viz Build
    Start the data visualization build for the conjecture. Install needed
    libraries and define the function along with initial shot at building
    it. Current version does not output the correct result, but the process
    itself does function properly.

--- collatz.py
+++ collatz.py
@@ -5,6 +5,8 @@ Collatz Conjecture Script
 Author: Matthew Sunner, 2022
 """
 
+import plotly.express as px
+import pandas as pd
 
 class Collatz():
     def __init__(self, input):
@@ -31,12 +33,37 @@ class Collatz():
                 n = self.even_func(n)
                 print(n)
 
+    def visualize_conjecture(self):
+        n = self.input
+        results = []
+        while n > 1:
+            if (n % 2):
+                n = self.odd_func(n)
+                results.append(results)
+            else:
+                n = self.even_func(n)
+                results.append(results)
+        
+        # Create a Dataframe From the Results
+        print(results)
+        df = pd.DataFrame(results)
+        print(df)
+
+        # Create Plot & Display Results
+
+        x = range(1,len(results)+1)
+        y = results
+
+        #fig = px.line(df, x=df.index, y=df['Result'])
+
+        #fig.show()
+
 
 def main():
     guess = int(input('Please Enter a Positive Number: '))
 
     c = Collatz(guess)
-    c.conjecture()
+    c.visualize_conjecture()
 
 
 if __name__ == '__main__':
